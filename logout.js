ce83cf80ee269cccccb6fbce9d33865f313def43
unknown <lsrezende1@gmail.com>
2026-05-29 17:35:50 -0300

    explicando o logout

--- logout.js
+++ logout.js
@@ -2,8 +2,14 @@
    LOGOUT (FUNÇÃO GLOBAL)
 ===================================== */
 function logout() {
+
+  
   localStorage.removeItem("logado");
+  /*aqui ele apaga a regra logado do loginform.js*/
   localStorage.removeItem("tipoUsuario");
+  /*aqui ele apaga o tipo de usuário*/
 
   window.location.href = "index.html";
+/*aqui ele volta para o inicio do arquivo index.html, que vai fazer novamente o login */
+
 }
